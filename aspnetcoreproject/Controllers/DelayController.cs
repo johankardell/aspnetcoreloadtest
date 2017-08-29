de25f192a3a3c5a6854334ff7ced5c0d0f408414
Johan Kardell <Johan.Kardell@collectorbank.se>
2017-08-29 10:45:54 +0200

    Increase thread count for each request to 100

--- aspnetcoreproject/Controllers/DelayController.cs
+++ aspnetcoreproject/Controllers/DelayController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,7 +10,14 @@ namespace aspnetcoreloadtest.Controllers
         [HttpGet]
         public async Task<string> Get()
         {
-            await Task.Delay(1000);
+            var tasks = new List<Task>();
+            for (var i = 0; i < 100; i++)
+            {
+                tasks.Add(Task.Run(async () => await Task.Delay(1000)));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
             return "Awake!" ;
         }
     }
