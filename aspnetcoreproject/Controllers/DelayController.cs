0e01a97e8283c489f2a29acff2a134c4e47eb109
Johan Kardell <johan.kardell@collectorbank.se>
2017-08-29 09:45:33 +0200

    Add controller with delay

---
+++ aspnetcoreproject/Controllers/DelayController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcoreloadtest.Controllers
+{
+    [Route("api/[controller]")]
+    public class DelayController : Controller
+    {
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            await Task.Delay(1000);
+            return "Awake!" ;
+        }
+    }
+}
